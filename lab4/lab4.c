91e78ccba2163a6fe3e040a332d33a3d6ac41686
hoybonbon1233 <kinghoop12345@gmail.com>
2025-07-22 18:14:46 +0700

    latest

---
+++ lab4/lab4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char EmployeesID[10] ;
+    int Hour = 0 ;
+    float salary = 0.00 ;
+        printf( " Enter the Employees ID : " ) ;
+        scanf( "%s", EmployeesID ) ;
+        printf( " Enter the working hrs: " ) ;
+        scanf( "%e",&salary ) ;
+        printf( " Enter the salary per hrs: " ) ;
+        scanf( "%d",&Hour ) ;
+        printf( " Expected Output:\n " ) ;
+        printf(" Employees ID = %s\n",EmployeesID ) ;
+        printf(" Salary amount/hr : %.2f\n ",Hour * salary ) ;
+    return 0 ;
+}//end
