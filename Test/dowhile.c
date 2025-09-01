bf63eec19f94299fd8fc27e28f3acc2f47ddd154
hoybonbon1233 <kinghoop12345@gmail.com>
2025-09-02 02:08:42 +0700

    zzz

--- Test/dowhile.c
+++ Test/dowhile.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 int main() {
     int n ;
-    printf("Enter Number: ") ;
-    scanf("%d", &n) ;
+    printf( "Enter Number: " ) ;
+    scanf( "%d",&n ) ;
 
     int i = n ;
     // วนจาก n ลงไปถึง 2
     do {
         int prime = 1 ;  // สมมติว่าเป็นจำนวนเฉพาะ
         int j = 2 ;
-
-        // ตรวจสอบตัวหาร
-        do {
+         
+        do {//ตรวจตัวหาร
             if  ( i % j == 0 ) {
                 prime = 0 ; // มีตัวหารลงตัว
                 break ;
             }
             j++ ;
-        } while (j <= i / 2) ;
-
-        if (prime && i >= 2) {
-            printf("%d ", i) ;
+        } while ( j <= i / 2 ) ;
+        if ( prime && i >= 2 ) {
+            printf( "%d ", i ) ;
         }
         i-- ; // ลดค่า i เพื่อตรวจต่อ
-    } while (i >= 2) ;
+    } while ( i >= 2 ) ;
     return 0 ;
 }
--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 int main() {
-    int score = 0;
-    printf("Please enter your score: ");
-    scanf("%d", &score);
-    printf("Grade: ");
+    int score = 0 ;
+    printf( "Please enter your score: " ) ;
+    scanf("%d", &score) ;
+    printf( "Grade: " ) ;
     if( score >= 80 ) {
-        printf("A !");
+        printf( "A !" ) ;
     } else if( score >= 75 && score < 80 ) {
-        printf("B+ !");
+        printf( "B+ !" ) ;
     } else if( score >= 70 && score < 75 ) {
-        printf("B !");
+        printf( "B !" ) ;
     } else if( score >= 65 && score < 70 ) {
-        printf("C+ !");
+        printf( "C+ !" ) ;
     } else if( score >= 60 && score < 65 ) {
-        printf("C ! ");
+        printf( "C ! " ) ;
     } else if( score >= 55 && score < 60 ) {
-        printf("D+ !");
+        printf( "D+ !" ) ;
     } else if( score >= 50 && score < 55 ) {
-        printf("D ! ");
+        printf( "D ! " ) ;
     } else {
-        printf("F !");
+        printf( "F !" ) ;
     } // end if
-    return 0;
+    return 0 ;
 } // end function
