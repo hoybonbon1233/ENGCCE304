b306a64310116d31f79e97d2c87d92bca3cdcf1b
hoybonbon1233 <kinghoop12345@gmail.com>
2025-09-02 01:54:09 +0700

    ,latetest

---
+++ สอสปฏิบัติ/while.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter Number: ");
+    scanf("%d", &n);
+
+    int i = n;
+    while (i >= 2) {
+        int prime = 1; // สมมติว่าเป็นจำนวนเฉพาะ
+        int j = 2;
+        while (j <= i / 2) {
+            if (i % j == 0) {
+                prime = 0; // ถ้ามีตัวหารลงตัว ไม่ใช่จำนวนเฉพาะ
+                break;
+            }
+            j++;
+        }
+        if (prime) {
+            printf("%d ", i);
+        }
+        i--;
+    }
+    return 0;
+}
