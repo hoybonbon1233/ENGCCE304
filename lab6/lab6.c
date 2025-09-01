8542537a20b8e865cc0d4568eeb2a82b312c7b4e
hoybonbon1233 <kinghoop12345@gmail.com>
2025-09-02 03:22:32 +0700

    zz

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -2,15 +2,15 @@
 int main() {
     int N ;
     printf(" Enter value: ") ;
-    scanf("%d",&N);
+    scanf("%d",&N) ;
 
     if( N % 2 == 1){
-        printf("Series: ");
+        printf("Series: ") ;
         for(int i = 1 ; i <= N ; i+= 2){
-            printf("%d ", i);
+            printf("%d ", i) ;
         }
-    } else{
-        printf("Series: ");
+    } else {
+        printf("Series: ") ;
         for(int i = N; i >= 0 ; i -= 2) {
             printf("%d ", i) ;
         }
