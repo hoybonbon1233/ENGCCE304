b3e9efd4d3257c5d244d38e858578e726d6b1ee4
hoybonbon1233 <kinghoop12345@gmail.com>
2025-10-10 22:26:41 +0700

    zz

--- lab3/Lab3.c
+++ lab3/Lab3.c
@@ -1,31 +1,12 @@
 #include <stdio.h>
-#include <math.h>
-
-int isPrime (int num)  {
-  if (num<2) return 0 ;
-  for (int i = 2 ; i <= sqrt(num) ; i++) {
-  if (num % i == 0)
-  return 0 ; }
-  return 1 ; }
-    int main() {
-    int n ;
-    printf(" Enter N : ") ;
-    scanf("%d",&n) ;
-    int arr[n] ; 
-        for(int i=0;i<n;i++) {
-        printf( " Enter value[%d] : ",i) ;
-        scanf("%d",&arr[i]) ; }
-        printf(" Index: ") ;
-          for (int i = 0;i < n;i++){
-          printf("%2d ", i) ; } 
-  printf("\n") ;
-  printf( " Array:  " ) ;
-    for(int i=0;i<n;i++){
-      if 
-      (isPrime(arr[i])) 
-      printf("%2d ", arr[i]) ; 
-      else
-      printf("%2s ", "#") ; }
-  printf("\n") ;
-  return 0 ;
-}//end function
+int main() {
+    int Height , Width ;
+    float Area;
+    printf("Enter Height : \n");
+    scanf("%d",&Height);
+    printf("Enter Width: \n");
+    scanf("%d",&Width);
+    Area = (Height*Width) / 2 ;
+    printf("Area is %d",Area);
+    return 0 ;
+}
